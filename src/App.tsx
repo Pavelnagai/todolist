5486282ae1aff3c9878d5215dfea03727eca2304
Pavelnagai <pavel.nagai@mail.ru>
2022-02-18 09:53:09 +0300

    changing the file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback} from 'react';
 import './App.css';
-import {TaskType, Todolist} from './Todolist';
-import {AddItemForm} from './AddItemForm';
+import {TaskType, Todolist} from './components/Todolist/Todolist';
+import {AddItemForm} from './components/AddItemForm/AddItemForm';
 import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from '@material-ui/core';
 import {Menu} from '@material-ui/icons';
 import {useDispatch, useSelector} from "react-redux";
--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,6 +1,7 @@
 import {combineReducers, createStore} from "redux";
 import {todolistsReducer} from "./todolists-reducer";
 import {tasksReducer} from "./tasks-reducer";
+import {TypedUseSelectorHook, useSelector} from "react-redux";
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 const rootReducer = combineReducers({
@@ -10,5 +11,8 @@ const rootReducer = combineReducers({
 
 
 export const store = createStore(rootReducer)
+
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+
 //@ts-ignore
 window.store = store
